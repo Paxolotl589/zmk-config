b4b7abc5831078367e8ec2318f34f1cc475435ee
Paxolotl589 <paxf.589@gmail.com>
2025-12-10 23:49:46 -0600

    Update wpm.c

--- boards/shields/nice_view_gem/widgets/wpm.c
+++ boards/shields/nice_view_gem/widgets/wpm.c
@@ -3,15 +3,15 @@
 #include "wpm.h"
 #include "../assets/custom_fonts.h"
 
-LV_IMG_DECLARE(gauge);
+// LV_IMG_DECLARE(gauge);
 LV_IMG_DECLARE(grid);
 
-static void draw_gauge(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_img_dsc_t img_dsc;
-    lv_draw_img_dsc_init(&img_dsc);
+// static void draw_gauge(lv_obj_t *canvas, const struct status_state *state) {
+//     lv_draw_img_dsc_t img_dsc;
+//     lv_draw_img_dsc_init(&img_dsc);
 
-    lv_canvas_draw_img(canvas, 16, 44 + BUFFER_OFFSET_MIDDLE, &gauge, &img_dsc);
-}
+//     lv_canvas_draw_img(canvas, 16, 44 + BUFFER_OFFSET_MIDDLE, &gauge, &img_dsc);
+// }
 
 static void draw_needle(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_line_dsc_t line_dsc;
